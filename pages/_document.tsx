04e193d3fe70ac9e4525f2bca41095e8bd5b8a43
Costin Mirica <costin@roninmobile.eu>
2019-06-17 16:06:46 +0300

    Fix og tags.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,7 @@ export default class MyDocument extends Document {
                     <meta property='og:url' content='https://costinmirica.com' />
                     <meta property='og:type' content='website' />
                     <meta property='og:title' content='Costin Mirica - Full Stack Developer' />
+                    <meta property='og:image' content='../static/costin-mirica.jpg' />
                     <meta property='og:description' content='Just another developer understading the world one line of code at a time.' />
                     <link rel='shortcut icon' href='../static/favicon.ico' />
                     <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css' />
