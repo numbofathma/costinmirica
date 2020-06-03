438f4a822d01030cc4a18feb676fa967ae90a372
Costin Mirica <costin@roninmobile.eu>
2020-06-03 09:46:38 +0300

    Add gzip compression.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,27 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
-// const withCSS = require('@zeit/next-css');
 const withTM = require('next-transpile-modules');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const nextConfig = {
   target: 'serverless',
-  // webpack(config, options) {
-  //     return config;
-  // }
+  webpack(config, options) {
+    config.plugins.push(
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg|png|jpg)$/,
+        threshold: 8192,
+        minRatio: 0.8
+      })
+    );
+
+    return config;
+  }
 };
 
 module.exports = withPlugins(
   [
-    // withCSS,
     withImages,
     [withTM, { transpileModules: [] }],
   ],
