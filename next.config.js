8bb0162e3383cf4b173c1b1b1aa1335c6a330687
Costin Mirica <costin.mirica@arnia.ro>
2024-01-24 17:42:50 +0200

    Damn forogot remotepatterns

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.costinmirica.com',
+        port: '',
+        pathname: '/static/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
