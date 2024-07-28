150ae97443eb318bf406e1901eef2bc617686cf8
Costin Mirica <costin.mirica@arnia.ro>
2024-07-28 11:05:02 +0300

    Fix request context issues in build step

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,9 +2,10 @@ import { headers } from 'next/headers';
 import { ISocial } from '@/interfaces/app';
 import { IProject } from '@/interfaces/app';
 
-const appUrl = headers().get('x-current-path');
-
 export const getProjects = async () => {
+  // Do not move headers outside request context
+  // https://nextjs.org/docs/messages/next-dynamic-api-wrong-context
+  const appUrl = headers().get('x-current-path');
   const response = await fetch(`${appUrl}/api/projects`);
   const { data = [], ok } = await response.json();
 
@@ -15,6 +16,9 @@ export const getProjects = async () => {
 };
 
 export const getSocials = async () => {
+  // Do not move headers outside request context
+  // https://nextjs.org/docs/messages/next-dynamic-api-wrong-context
+  const appUrl = headers().get('x-current-path');
   const response = await fetch(`${appUrl}/api/socials`);
   const { data = [], ok } = await response.json();
 
