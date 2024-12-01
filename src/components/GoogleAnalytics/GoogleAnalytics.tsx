5576f4be71cf035da3cfec058b3cd5e0a203c83a
Costin Mirica <costin.mirica@arnia.ro>
2024-12-01 18:11:48 +0200

    Add cookieyes

--- src/components/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -8,6 +8,7 @@ interface IGoogleAnalyticsProps extends ScriptProps {
 const GoogleAnalytics: FC<IGoogleAnalyticsProps> = ({ gaId }) => {
   return (
     <>
+      <Script id='cookieyes' type='text/javascript' src='https://cdn-cookieyes.com/client_data/fdf3173583a2e15614a9a4da/script.js' />
       <Script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} defer strategy='lazyOnload' />
       <Script id='google-analytics' defer strategy='lazyOnload'>
         {`
