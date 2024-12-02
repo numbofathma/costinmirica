7b0e2a80904007b28818fc1d040db04df05a23dc
Costin Mirica <costin.mirica@arnia.ro>
2024-12-02 22:20:04 +0200

    Remove Cookiebot service

--- src/components/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Script from 'next/script';
-import { COOKIEBOT_ID, GA_ID } from '@/constants';
+import { GA_ID } from '@/constants';
 
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script id='Cookiebot' src='https://consent.cookiebot.com/uc.js' data-cbid={COOKIEBOT_ID} data-blockingmode='auto' type='text/javascript' />
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} defer strategy='lazyOnload' />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} defer strategy='lazyOnload' />
       <Script id='google-analytics' defer strategy='lazyOnload'>
         {`
         window.dataLayer = window.dataLayer || [];
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const DEV_MODE = process.env.NODE_ENV === 'development';
 export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || '';
-export const COOKIEBOT_ID = process.env.NEXT_PUBLIC_COOKIEBOT_ID || '';
